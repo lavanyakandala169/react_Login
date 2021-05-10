33bb1cf709a21721f039257c4e2c11615b5c424e
nagarjunam1 <nagarjunam1@verifone.com>
2021-05-10 22:31:51 +0530

    first Login

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import LoginForm from './components/LoginForm'
+import './App.css'
+
+function App() {
+  const adminUser={
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = ("");
+  const Login = details => {
+    console.log(details);
+
+    if(details.email == adminUser.email && details.password == adminUser.password){
+    console.log("Logged in");
+    setUser({
+      name: details.name,
+      email: details.email
+    })
+  } else {
+console.log("Detailes do not match!");
+setError("Detailes do not match!");
+  }
+}
+  const Logout = () =>{
+    setUser({name: "", email:""})
+  }
+  return (
+    <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ):(
+        <LoginForm Login={Login} error={error}/>
+      )} 
+    </div>
+  )
+}
+
+export default App
